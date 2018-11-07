5b34bfb4d26d7009cae9c258a3607cd1591f755d
korr <kimberly.orr97@gmail.com>
2018-11-07 13:22:22 -0600

    Tests: Service Times and Interarrival Times validated.
    The means and variances of service times and interarrival times are as expected.

--- test/TrafficGeneratorTest.java
+++ test/TrafficGeneratorTest.java
@@ -7,10 +7,10 @@ class TrafficGeneratorTest {
 
     @Test
     /**
-     * Tests that interarrival times follow a Poisson Process.
+     * Tests that interarrival times follow a Poisson Process (exponentially distributed).
      */
     void interarrivalTimes(){
-        int numConnections = 10000;
+        int numConnections = 100000;
         Connection[] startConnects = new Connection[numConnections];
         double[] arrivals = new double[numConnections];
 
@@ -28,56 +28,63 @@ class TrafficGeneratorTest {
         }
 
         /**
-         * Calculate mean of arrival times.
+         * Calculate mean and variance of arrival times.
+         * Sample mean = sum of all of the observations divided by the number of observations
+         * Sample variance = (sum of (x_i - mean)^2) / (n-1)
          */
         double sum = 0;
-        double variance = 0;
         for(int i=0, j=1; j<arrivals.length; i++, j++){
             sum += arrivals[j]-arrivals[i];
-            variance += ((arrivals[j]-arrivals[i])-(sum/j))*((arrivals[j]-arrivals[i])-(sum/j)); // need to check (esp for off by one)
-//            if(i>500) {
-//                assertEquals(sum, i * 2, 1);
-//            }
         }
         double mean = sum / arrivals.length;
-        variance = variance/arrivals.length; //should be avg arrival time ** 2
-        System.out.println(variance);
+
+        //calculate variance
+        double variance = 0;
+        for(int i=0, j=1; j<arrivals.length; i++, j++){
+            variance += ((arrivals[j]-arrivals[i])-mean)*((arrivals[j]-arrivals[i])-mean); //sum (x_i - E[x])^2
+        }
+        variance = variance/(arrivals.length-1);
 
         //check that the actual average interarrival time is approximately equal to avgArrivalTime
         assertEquals(mean, 2, .05);
+        assertEquals(variance, 2*2, .05);
         //check that the nth arrival is approximately Gamma(n, lambda) (IN FOR LOOP)
         //check that it follows a poisson process: s = E[N(s)]/lambda
     }
-//    @Test
-//    void getArrivals() {
-//    }
-//
-//    @Test
-//    void generateArrivals() {
-//        double avgArrivalTime = 4;
-//        TrafficGenerator obs100000 = new TrafficGenerator(100000, avgArrivalTime);
-//        double[] arrivals = obs100000.getArrivals();
-//        double sum = 0;
-//        int j = 1;
-//        for(int i=0; i<arrivals.length-1; i++){
-//            sum += (arrivals[j]-arrivals[i]);
-//            j++;
-//        }
-//        double mean = sum / arrivals.length;
-//        assertTrue(mean > avgArrivalTime - 0.25 && mean < avgArrivalTime + 0.25);
-//    }
-//
-//    @Test
-//    void generateConnections() {
-//        double avgServiceTime = 3;
-//        TrafficGenerator obs100000 = new TrafficGenerator(100000, 5, avgServiceTime);
-//        double[] connections = obs100000.getConnectionLengths();
-//        double sum = 0;
-//        for(double i : connections)
-//            sum += i;
-//        double mean = sum / connections.length;
-//        assertTrue(mean > avgServiceTime - 0.25 && mean < avgServiceTime + 0.25);
-//    }
+
+    @Test
+    /**
+     * Test to check that the service times have the same average as requested.
+     */
+    void serviceTimes(){
+        TrafficGenerator gen = new TrafficGenerator(2, 3, 1, 16);
+        int numConnections = 100000;
+        int[] nodeList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double[] serviceTimes = new double[numConnections];
+
+        double sumServiceTimes = 0;
+        double varianceServTimes = 0;
+        double prevTime=0;
+        for(int i=0; i<serviceTimes.length; i++){
+            //Generate connections
+            Connection start = gen.newConnectionStart(i, prevTime, nodeList);
+            prevTime = start.getTime();
+            Connection end = gen.newConnectionEnd(start);
+            double serviceTime = end.getTime() - start.getTime();
+            serviceTimes[i] = serviceTime;
+            sumServiceTimes += serviceTime;
+        }
+        double avgServiceTime = sumServiceTimes / numConnections;
+
+        for(int i=0; i<serviceTimes.length; i++){
+            varianceServTimes += (serviceTimes[i] - avgServiceTime)*(serviceTimes[i] - avgServiceTime);
+        }
+        varianceServTimes = varianceServTimes / (numConnections - 1);
+
+        assertEquals(avgServiceTime, 3, .05);
+        assertEquals(varianceServTimes, 3*3, .05);
+    }
+
 //
 //    @Test
 //    void generateSrcAndDest() {
